4aa47d9f44fbd84477d31e523cc4891afade7335
Kyle Button <154020767+TakoSalads@users.noreply.github.com>
2025-02-27 01:24:23 -0500

    spotify API project
    * currently takes last 10 songs listened too and the current song being listened too and adds them to a playlist together with duplicate check.

--- personal-projects/spotify/main.py
+++ personal-projects/spotify/main.py
@@ -11,6 +11,14 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
 scopes = [
     "user-read-currently-playing",
@@ -19,23 +27,61 @@ scopes = [
     "playlist-read-private"
 ]
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="41672c6c7c034a7b8c39f7c8af6bb467",      # Both require setting up
-                                               client_secret="081c4f7b7cee46b393ad4022c234d560",  # spotify developer
-                                               redirect_uri="http://localhost:8888/callback",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,      # Both require setting up
+                                               client_secret=client_secret,  # spotify developer
+                                               redirect_uri=redirect_uri,
                                                scope=" ".join(scopes)))
 
-current_track = sp.current_user_playing_track()
-if current_track:
-    print("Success! Currently playing:", current_track["item"]["name"])
-else:
-    print("Failed! - no song playing or utter failure")
+#playlist ID
+playlist_id = "0uqbFM1bOiBWQhxcBOSKyk"
 
-recently_played = sp.current_user_recently_played(limit=10, after=None, before=None)
-for idx, item in enumerate(recently_played["items"]):
-    track = item["track"]
-    song_name = track["name"]
-    artist_name = track["artists"][0]["name"]
-    played_at = item["played_at"] 
+#store current tracks
+track_uris = []
 
-    print(f"{idx+1}. {song_name} by {artist_name} - (Played at: {played_at})")
+#gathering songs
+def get_current_track():
+    current_track = sp.current_user_playing_track()
+    if current_track and current_track["item"]:
+        track_uri = current_track["item"]["uri"]
+        track_uris.append(track_uri)
+        print("Success! Currently playing:", current_track["item"]["name"])
+    else:
+        print("Failed! - no song playing or utter failure")
+
+def get_recent():
+    recently_played = sp.current_user_recently_played(limit=10, after=None, before=None)
+    new_tracks = [item["track"]["uri"] for item in recently_played["items"]]
+    return new_tracks
+
+def combination():
+    track_uris.extend(get_recent())
+
+#adding to playlist
+def add_items():
+    if track_uris:
+        noDupe = list(dict.fromkeys(track_uris))
+
+        existing_tracks = sp.playlist_tracks(playlist_id)["items"]
+        existing_urls = [item["track"]["uri"] for item in existing_tracks]
+
+        new_tracks = [track for track in noDupe if track not in existing_urls]
+
+        if new_tracks:
+            sp.playlist_add_items(playlist_id=playlist_id, items=noDupe)
+            print(f"Added {len(noDupe)} tracks to the playlist!")
+        else:
+            print("No new tracks to add.")
+    else:
+        print("GFYS")
+
+        
+
+
+get_current_track()
+get_recent()
+combination()
+add_items()
+
+
+
 
